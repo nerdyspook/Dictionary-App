a8cc80239ae518f6317ec74e4b4ad791a0e06426
Susanto <nerdyspook007@gmail.com>
2021-11-02 13:10:53 +0530

    feat: search field added

--- js/main.js
+++ js/main.js
@@ -3,8 +3,30 @@ const searchInput = document.querySelector("input");
 const searchBtn = document.querySelector("#search-btn");
 const infoText = document.querySelector(".info");
 
-function fetchApi(word) {
+function details(result, word){
+    console.log(result);
+
+    infoText.classList.remove("failed");
     
+
+    if(result.title){
+        infoText.classList.add("failed");
+        infoText.innerHTML = result.message;
+    }else {
+        infoText.classList.remove("search");
+    }
+}
+
+function fetchApi(word) {
+    infoText.style.color = "#1F2937";
+    infoText.classList.add("search");
+    infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>` 
+
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(result => details(result, word));
 };
 
 searchInput.addEventListener("keyup", e =>{
